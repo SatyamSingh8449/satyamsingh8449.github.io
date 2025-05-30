73ca4e62ffda62e5cdc2c3e58b82a6ea9bf50f1d
satyamsingh8449 <Satyamsingh8449@gmail.com>
2025-05-30 18:32:11 +0530

    Chnages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/satyamsingh8449.github.io/",
+  base: "/",
   plugins: [react()],
 })
